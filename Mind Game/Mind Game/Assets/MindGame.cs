17e8f92a2e0887b1c76307089d979d8aa4e272f2
kamrul hasan <kamrul.securesoft@gmail.com>
2020-09-26 23:10:13 +0600

    Create a executable game name mind game

---
+++ Mind Game/Mind Game/Assets/MindGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MindGame : MonoBehaviour {
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.getStory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        manageState();
+	}
+
+    private void manageState()
+    {
+        var nextState = state.getNextState();
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            state = nextState[0];
+        }else if (Input.GetKeyDown(KeyCode.P))
+        {
+            state = nextState[1];
+        }
+        else
+        {
+            
+        }
+        textComponent.text = state.getStory();
+    }
+}
